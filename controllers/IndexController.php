41c91ee4acbdff861625d3a770e1d2d6b7116328
Eric C. Weig <eweig@email.uky.edu>
2018-12-18 12:42:00 -0500

    Create IndexController.php

---
+++ controllers/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @copyright Eric C. Weig, 2018
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ * @package SimpleCart
+ */
+
+/**
+ * Controller for Cart form.
+ *
+ * @package SimpleCart
+ */
+class SimpleCart_IndexController extends Omeka_Controller_AbstractActionController
+{
+    public function indexAction()
+    {
+ 
+    }
+}
