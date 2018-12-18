4a4b397141e973506f2238d2395a8db5ee34c4e8
Eric C. Weig <eweig@email.uky.edu>
2018-12-18 12:45:17 -0500

    Create language-en.js

---
+++ views/public/javascripts/language-en.js
@@ -0,0 +1,20 @@
+//                      Language Strings                               ||
+strSorry  = "I'm Sorry, your requests list is full.";
+strAdded  = "added";
+strRemove = "Click 'Ok' to remove this interview from your list.";
+strILabel = "Product Id";
+strDLabel = "Product Name/Description";
+strQLabel = "Quantity";
+strPLabel = "Price";
+strSLabel = "Shipping";
+strRLabel = "Remove From List";
+strRButton= "Remove";
+strSUB    = "SUBTOTAL";
+strSHIP   = "SHIPPING";
+strTAX    = "TAX";
+strTOT    = "TOTAL";
+strErrQty = "Invalid Quantity.";
+strNewQty = 'Please enter new quantity:';
+
+Language = 'en';
+bLanguageDefined = true;
